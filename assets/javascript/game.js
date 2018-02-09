81cad45ece6d1dc92947d0504d416784dfebde49
Jamie Marie Burgess <jbmariebg4@gmail.com>
2018-02-09 17:37:55 -0600

    added prompt and messages, additional button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,23 @@
 
 function changeBackground(){
-   // document.body.style.backgroundColor="darkgray"; 
+   var colors = ["lightyellow", "pink", "lightgreen" , "lightblue" , "lavender"];
 
-
-var colors = ["lightyellow", "pink", "lightgreen" , "lightblue"];
+    //for(i=0; i<colors.length; i++){document.body.style.backgroundColor=(colors[i])} ?
     
-document.body.style.backgroundColor=colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor=colors[Math.floor(Math.random() * colors.length)];
+};
+
+function userGuess(){
+    var correctLetters=["f","u","n"];
+    var guess=prompt("Pick a letter.");
+    
+    
+    for (i=0; i<correctLetters.length; i++);
+    if (guess===correctLetters[i]){
+        alert("That's correct!");
+    }else{
+        alert("Incorrect. Please try again.");
+    }
 }
+        //getElementById:"column4LettersGuessed" document.write(guess);
+        
